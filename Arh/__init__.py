9c14edd54cde7f757b57f601140f34c3b24598a3
CaptAlpha <arhitbose.tagore2020@vitstudent.ac.in>
2022-01-06 19:13:36 +0530

    📁 Added Content

---
+++ Arh/__init__.py
@@ -0,0 +1,14 @@
+
+from setuptools import setup
+
+setup(
+    name='yourapplication',
+    packages=['yourapplication'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
+
+
+        
--- main.py
+++ main.py
@@ -35,6 +35,51 @@ def flask(name):
         if not os.path.exists(path+'/static'):
             os.makedirs(path+'/static')
 
+        file = open(path+"/__init__.py", "w")
+        file.write("""
+from setuptools import setup
+
+setup(
+    name='yourapplication',
+    packages=['yourapplication'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
+
+
+        """)
+        file.close()
+
+        file = open(path+"/app.py", "w")
+        file.write("""
+from flask import Flask
+
+# Flask constructor takes the name of 
+# current module (__name__) as argument.
+app = Flask(__name__)
+
+# The route() function of the Flask class is a decorator, 
+# which tells the application which URL should call 
+# the associated function.
+@app.route('/')
+# ‘/’ URL is bound with hello_world() function.
+def hello_world():
+    return 'Hello World'
+
+# main driver function
+if __name__ == '__main__':
+
+    # run() method of Flask class runs the application 
+    # on the local development server.
+    app.run()
+        """)
+    
+
+        file.close()
+        
+
     except OSError as error:
         print(error)
         
