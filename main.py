abfa0e297bf32da74b7c4af40b1cc4655c8ef00f
mekken2 <priyanshu.kumar2021b@vitstudent.ac.in>
2022-01-25 00:54:12 +0530

    Added Flutter

--- main.py
+++ main.py
@@ -204,20 +204,20 @@ def angular(name):
         if not os.path.exists(path):
             os.makedirs(path)
 
-        arr_react1 = ['/src','/node_modules','/src/app','/src/assets','/src/environments']
+        arr_angular1 = ['/src','/node_modules','/src/app','/src/assets','/src/environments']
 
-        for i in arr_react1:
+        for i in arr_angular1:
             if not os.path.exists(path + i):
                 os.makedirs(path + i)
 
-        arr_react2 = ['/.editorconfig', '/.gitignore', '/README.md',
+        arr_angular2 = ['/.editorconfig', '/.gitignore', '/README.md',
                       '/angular.json', '/package.json', '/package-lock.json',
                       '/tsconfig.json', '/src/index.html', '/src/main.ts',
                       '/src/polyfills.ts', '/src/styles.sass', '/src/test.ts',
                       '/src/app/app.component.ts', '/src/app/app.component.html', '/src/app/app.component.css',
                       '/src/app/app.component.spec.ts','/src/app/app.module.ts']
 
-        for i in arr_react2:
+        for i in arr_angular2:
             file = open(path + i, "w")
             file.write(""" . """)
             file.close()
@@ -225,8 +225,36 @@ def angular(name):
     except OSError as error:
         print(error)
 
-# def flutter(lst):
-#     pass
+def flutter(name):
+    """ Flutter entry point of the app """
+    cwd = os.getcwd()
+    directory = name
+    path = cwd + "/" + directory
+
+    try:
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        arr_flutter1 = ['/views','/views/auth','/views/widgets','/model','/net','/assets','/assets/flares','/assets/fonts']
+
+        for i in arr_flutter1:
+            if not os.path.exists(path + i):
+                os.makedirs(path + i)
+
+        arr_flutter2 = ['/views/main.dart', '/views/opening_view.dart', '/views/home_view.dart',
+                      '/views/auth/login_view.dart', '/views/register_view.dart', '/views/widgets/custom_alert_dialog.dart',
+                      '/views/widgets/custom_tab_bar.dart', '/model/user_model.dart', '/model/item_model.dart',
+                      '/model/method_request_model.dart', '/model/method_response_model.dart', '/net/user_data_api.dart',
+                      '/net/get_items_api.dart', '/net/firebase.dart', '/assets/flares/flare1.flr',
+                      '/assets/fonts/font1.ttf']
+
+        for i in arr_flutter2:
+            file = open(path + i, "w")
+            file.write(""" . """)
+            file.close()
+
+    except OSError as error:
+        print(error)
 
 # def rubyonrails(lst):
 #     pass
@@ -261,6 +289,9 @@ def run(type, name):
 
     if type.casefold() == 'react':
         react(name)
+
+    if type.casefold() == 'angular':
+        angular(name)
 
 
 if __name__ == "__main__":
